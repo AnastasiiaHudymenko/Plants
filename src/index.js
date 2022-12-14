50c3a0ac5df04319972c43e565e0cb22d226ed7f
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2022-12-14 13:56:31 +0200

    add section proposition html and css

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// const refs = {
+//   form: document.querySelector('.form'),
+//   ul: document.querySelector('.list'),
+//   btnLoadMore: document.querySelector('.load-more'),
+//   nav: document.querySelector('.nav-menu'),
+//   btnRegist: document.querySelector('button[data-action="regist"]'),
+// };
+// const data = {};
+// regs.form.addEventListener('submit', onSubmitForm);
+
+// function onSubmitForm(evt) {
+//   if (!evt.target) {
+//     return;
+//   }
+//   if (data[evt.target.name.value]) {
+//     data[evt.target.name] = JSON.stringify(evt.target.name.value);
+//   }
+// }
