56b225b2bd83129fd77a9c0dc1f28d53e6f6733b
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2022-12-21 08:36:36 +0000

    Deploying to gh-pages from @ AnastasiiaHudymenko/plants@cc84673e594a6ba5f374407f473995f7d353babb 🚀

--- index.dd00ab80.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".btn-open-js"),c=document.querySelector(".backdrop"),n=document.querySelector(".btn-close-js");e.addEventListener("click",(function(){c.classList.add("is-open")})),n.addEventListener("click",(function(){c.classList.remove("is-open")}));
-//# sourceMappingURL=index.dd00ab80.js.map
